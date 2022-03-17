2a94f3df79cbede7dc25b8a96d6e7751102c6bd2
tetris.dev.web <tetris.dev.web@gmail.com>
2022-03-17 16:10:44 +0800

    remove

--- config.php
+++ config.php
@@ -1,8 +1,9 @@
 <?php
 require('./vendor/autoload.php');
+define('FCPATH', dirname(__FILE__).'/');
 
-$dotenv = Dotenv\Dotenv::createImmutable('./');
-$dotenv->load('.env');
+$dotenv = Dotenv\Dotenv::createImmutable(FCPATH);
+$dotenv->load(FCPATH . '.env');
 
 $config['SHOPIFY_APP_HOST_NAME'] = 'furryfreshness.myshopify.com';
 $config['SHOPIFY_API_VERSION'] = '2021-04';
@@ -11,9 +12,9 @@ Shopify\Context::initialize(
   $_ENV['SHOPIFY_API_KEY'],
   $_ENV['SHOPIFY_API_SECRET'],
   $_ENV['SHOPIFY_APP_SCOPES'],
-  $this->config->item('SHOPIFY_APP_HOST_NAME'),
+  $config['SHOPIFY_APP_HOST_NAME'],
   new Shopify\Auth\FileSessionStorage(FCPATH . 'tmp/php_sessions'),
-  $this->config->item('SHOPIFY_API_VERSION'),
+  $config['SHOPIFY_API_VERSION'],
   false,
   true,
   $_ENV['SHOPIFY_STOREFRONT_ACCESS_TOKEN']
